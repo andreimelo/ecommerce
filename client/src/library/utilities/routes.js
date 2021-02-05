6b463295478cf7b0efa629577a3e1400591cec93
andreimelo <meloandrei69@gmail.com>
2021-02-05 20:52:33 +0800

    refactor : setup hooks and routes

--- client/src/library/utilities/routes.js
+++ client/src/library/utilities/routes.js
@@ -1,19 +1,19 @@
-import { lazy } from 'react';
+import {lazy} from 'react';
 const Home = lazy(() => import('../../modules/default/Home'));
 const Login = lazy(() => import('../../modules/auth/Login'));
 const Register = lazy(() => import('../../modules/auth/Register'));
 
 export const route = [
-    {
-        path: '/',
-        component : Home
-    }, 
-    {
-        path: "/login",
-        component:  Login,
-    },
-    {
-        path:"/register",
-        component : Register
-    }
+	{
+		path      : '/',
+		component : Home,
+	},
+	{
+		path      : '/login',
+		component : Login,
+	},
+	{
+		path      : '/register',
+		component : Register,
+	},
 ];
