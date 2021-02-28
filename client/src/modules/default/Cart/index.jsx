37d16160f9fa05b37956f9160d1e3ee3aec6357c
andreimelo <andreiagno.melo@gmail.com>
2021-02-28 18:33:59 +0800

    chore: add file for shop and cart

---
+++ client/src/modules/default/Cart/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Cart(){
+	return <div> Welcome to Cart</div>;
+}
+
+export default Cart;
---
+++ client/src/modules/default/Shop/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Shop(){
+	return <div> Welcome to Shop</div>;
+}
+
+export default Shop;
