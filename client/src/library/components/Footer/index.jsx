6446a2814c357c156a2b2f563c3b2011c4aa5883
andreimelo <andreiagno.melo@gmail.com>
2021-03-08 23:00:52 +0800

    refactor: align code format

--- client/src/library/components/Footer/index.jsx
+++ client/src/library/components/Footer/index.jsx
@@ -3,7 +3,10 @@ import './style.css'
 import '../../../resources/styles/global.css';
 
 function Footer(){
-	return <footer className="footer-container"><div className="layout-default">This is footer</div></footer>;
+	return (
+		<footer className="footer-container">
+			<div className="layout-default">This is footer</div>
+		</footer>);
 }
 
 export default Footer;
