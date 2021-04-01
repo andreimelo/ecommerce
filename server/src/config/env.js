5d386f86db6fab588ab287c3292e4afea0ed2b34
andreimelo <andreiagno.melo@gmail.com>
2021-04-01 13:42:24 +0800

    refactor : add or in port env

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-	port     : process.env.PORT,
+	port     : process.env.PORT || 8000,
 	database : {
 		uri : process.env.MONGODB_URI,
 	},
