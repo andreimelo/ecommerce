0a0f79111262ca6d2d62ffded8a8715b9082df02
andreimelo <andreiagno.melo@gmail.com>
2021-04-02 17:12:52 +0800

    chore : add current user route

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -5,21 +5,38 @@ exports.defaultAuthRoute = (req, res) => {
 };
 
 exports.createOrUpdateUser = async (req, res) => {
-	const { name, picture, email } = req.user;
+	try {
+		const { name, picture, email } = req.user;
 
-	const user = await User.findOneAndUpdate({ email }, { name, picture }, { new: true });
+		const user = await User.findOneAndUpdate(
+			{ email },
+			{ name, picture },
+			{ new: true },
+		);
 
-	if (user) {
-		console.log(user, 'existing user');
-		return res.json(user);
+		if (user) {
+			console.log(user, 'existing user');
+			return res.json(user);
+		}
+		else {
+			const newUser = await new User({
+				email,
+				name,
+				picture,
+			}).save();
+			console.log(newUser, 'new user');
+			return res.json(newUser);
+		}
+	} catch (error) {
+		return error;
 	}
-	else {
-		const newUser = await new User({
-			email,
-			name,
-			picture,
-		}).save();
-		console.log(newUser, 'new user');
-		return res.json(newUser);
-	}
+};
+
+exports.currentUser = async (req, res) => {
+	User.findOne({ email: req.user.email }).exec((err, user) => {
+		if (err) {
+			throw new Error(err);
+		}
+		res.json(user);
+	});
 };
--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,9 +1,15 @@
 const { router } = require('../config/express');
-const { defaultAuthRoute, createOrUpdateUser } = require('../controllers/auth');
+const {
+	defaultAuthRoute,
+	createOrUpdateUser,
+	currentUser,
+} = require('../controllers/auth');
 const { authCheck } = require('../middlewares/authSecurity');
 
 router.get('/auth', defaultAuthRoute);
 
 router.post('/auth/create-or-update', authCheck, createOrUpdateUser);
+
+router.post('/auth/current-user', authCheck, currentUser);
 
 module.exports = router;
