5baad8305909e9a5065449485e966488d5be8c34
andreimelo <andreiagno.melo@gmail.com>
2021-04-04 15:30:33 +0800

    chore : add admin middleware and route

--- server/src/middlewares/authSecurity.js
+++ server/src/middlewares/authSecurity.js
@@ -1,4 +1,5 @@
 const admin = require('../config/firebase');
+const User = require('../models/user');
 
 exports.authCheck = async (req, res, next) => {
 	let { authtoken } = req.headers;
@@ -6,6 +7,26 @@ exports.authCheck = async (req, res, next) => {
 		const firebaseUser = await admin.auth().verifyIdToken(authtoken);
 		console.log(firebaseUser);
 		req.user = firebaseUser;
+		next();
+	} catch (error) {
+		res.status(401).json({
+			error,
+		});
+	}
+};
+
+exports.adminCheck = async (req, res, next) => {
+	try {
+		let { email } = req.user;
+		const { role } = await User.findOne({ email }).exec();
+		if (role === 'admin') {
+			next();
+		}
+		else {
+			re.status(403).json({
+				message : 'Admin resource, access denied',
+			});
+		}
 		next();
 	} catch (error) {
 		res.status(401).json({
--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -4,12 +4,14 @@ const {
 	createOrUpdateUser,
 	currentUser,
 } = require('../controllers/auth');
-const { authCheck } = require('../middlewares/authSecurity');
+const { authCheck, adminCheck } = require('../middlewares/authSecurity');
 
 router.get('/auth', defaultAuthRoute);
 
 router.post('/auth/create-or-update', authCheck, createOrUpdateUser);
 
 router.post('/auth/current-user', authCheck, currentUser);
+
+router.post('/auth/current-admin', adminCheck, currentUser);
 
 module.exports = router;
