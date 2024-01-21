a5e1aaaca09c28dc6747f261125d472030a9a5e0
andreimelo <andreiagno.melo@gmail.com>
2024-01-21 01:55:31 -0600

    refactor: Adjustment in header css - nav container

--- client/src/library/components/Header/index.jsx
+++ client/src/library/components/Header/index.jsx
@@ -7,54 +7,93 @@ import CustomInput from '../Input';
 import './style.css';
 import { logOutAction } from '../../common/actions/authentication';
 
-function Header() {
+function Header(role) {
 	
 	const history = useHistory();
 	const dispatch = useDispatch();
-	const user = useSelector(({user}) => user)
-	
-	return (
-		<nav className='nav-container shadow'>
-			<div className='nav-sub-container layout-default'>
-				<div id='logo'>{string.common.logoTitle}</div>
-					<div className="position-right">
-					<div className="nav-title" onClick={() => history.push('/')}>
-						{string.routes.homeTitle}
-					</div>
-					<div className="nav-title" onClick={() => history.push('/shop')}>{string.navigation.shopTitle}</div>
-					<div className="nav-title" onClick={() => history.push('/cart')}>{string.navigation.cartTitle}</div>
-					{/* Input */}
-					<CustomInput type={type.input.search} name={"search"} placeHolder={"Search"} variant="inp fix-size" onChange={(event)=>console.log(event)}/>
-					{ user && 
-						(<div className="settings">
-							<div className="nav-title list">
-							{(user && user.email) || (!user && string.routes.userNamePlaceHolderTitle)}
-							</div>
-							<div className="settings-content" onClick={() => logOutAction(history, dispatch)} >{string.routes.logOutTitle}</div>
-						</div>)
-					}
-					{!user &&
-						(
-						<>
-						<div className="nav-title" onClick={() => history.push('/login')}>
-							{string.routes.loginTitle}
+    const { user } = useSelector((state) => ({ ...state }));
+
+	function renderRoleHeader(role) {
+		switch (role) {
+			case 'admin':
+				return (
+					<nav className='nav-container shadow'>
+					<div className='nav-sub-container layout-default'>
+						<div id='logo'>{string.common.logoTitle}</div>
+						<div className="nav-link-container">
+							{/* Input */}
+							{user &&
+								(<div className="settings">
+									<div className="nav-title list">
+										{(user && user.email) || (!user && string.routes.userNamePlaceHolderTitle)}
+									</div>
+									<div className="settings-content" onClick={() => logOutAction(history, dispatch)} >{string.routes.logOutTitle}</div>
+								</div>)
+							}
+							{!user &&
+								(
+									<>
+										<div className="nav-title" onClick={() => history.push('/login')}>
+											{string.routes.loginTitle}
+										</div>
+										<div className="nav-title" onClick={() => history.push('/register')}>
+											{string.routes.registerTitle}
+										</div>
+									</>
+								)
+							}
 						</div>
-						<div className="nav-title" onClick={() => history.push('/register')}>
-							{string.routes.registerTitle}
+					</div>
+				</nav>
+				);
+			default:
+				return (
+					<nav className='nav-container shadow'>
+						<div className='nav-sub-container layout-default'>
+							<div id='logo'>{string.common.logoTitle}</div>
+							<div className="nav-link-container">
+								<div className="nav-title" onClick={() => history.push('/')}>
+									{string.routes.homeTitle}
+								</div>
+								<div className="nav-title" onClick={() => history.push('/shop')}>{string.navigation.shopTitle}</div>
+								<div className="nav-title" onClick={() => history.push('/cart')}>{string.navigation.cartTitle}</div>
+								{/* Input */}
+								<CustomInput type={type.input.search} name={"search"} placeHolder={"Search"} variant="inp fix-size" onChange={(event) => console.log(event)} />
+								{user &&
+									(<div className="settings">
+										<div className="nav-title list">
+											{(user && user.email) || (!user && string.routes.userNamePlaceHolderTitle)}
+										</div>
+										<div className="settings-content" onClick={() => logOutAction(history, dispatch)} >{string.routes.logOutTitle}</div>
+									</div>)
+								}
+								{!user &&
+									(
+										<>
+											<div className="nav-title" onClick={() => history.push('/login')}>
+												{string.routes.loginTitle}
+											</div>
+											<div className="nav-title" onClick={() => history.push('/register')}>
+												{string.routes.registerTitle}
+											</div>
+										</>
+									)
+								}
 							</div>
-						</>
-						)
-					}
-				</div>
-			</div>
-			<div className='nav-sub-container layout-default'>
-				<div className="nav-title category">{string.navigation.categoriesTitle}</div>
-				<div className="nav-title category">
-					{string.navigation.subCategoriesTitle}
-				</div>
-			</div>
-		</nav>
-	);
+						</div>
+						{/*  .category
+					<div className='nav-link-container'>
+					<div className="nav-title category">{string.navigation.categoriesTitle}</div>
+					<div className="nav-title category">
+						{string.navigation.subCategoriesTitle}
+					</div>
+				</div> */}
+					</nav>
+				);
+		}
+	}
+
+	return renderRoleHeader(role)
 }
 
 export default React.memo(Header);
