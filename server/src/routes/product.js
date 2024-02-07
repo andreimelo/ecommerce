51f0e4fdb2d6fa753f0d2872b85b156e1477e6d3
andreimelo <andreiagno.melo@gmail.com>
2024-02-07 16:52:29 -0600

    feat(product): Added get all products in server

--- server/src/routes/product.js
+++ server/src/routes/product.js
@@ -1,8 +1,9 @@
 const { router } = require('../config/express');
 const { authCheck, adminCheck } = require('../middlewares/authSecurity');
-const { create } = require('../controllers/product');
+const { create, read } = require('../controllers/product');
 
 //routes
 router.post('/product', authCheck, adminCheck, create);
+router.get('/products', read);
 
 module.exports = router;
