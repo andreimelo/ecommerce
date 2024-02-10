a5a810d95dbeb25bf81fe4cc05a1fd99b5d55104
andreimelo <andreiagno.melo@gmail.com>
2024-02-10 13:23:40 -0600

    feat(products): Added endpoint for get all products in server

--- server/src/routes/product.js
+++ server/src/routes/product.js
@@ -1,9 +1,9 @@
 const { router } = require('../config/express');
 const { authCheck, adminCheck } = require('../middlewares/authSecurity');
-const { create, read } = require('../controllers/product');
+const { create, list } = require('../controllers/product');
 
 //routes
 router.post('/product', authCheck, adminCheck, create);
-router.get('/products', read);
+router.get('/products/:count', list);
 
 module.exports = router;
