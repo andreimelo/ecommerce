ceffe7bebda8a308b505e6cd97cd6416ab00d5e9
andreimelo <andreiagno.melo@gmail.com>
2024-02-19 15:39:14 -0600

    refactor(footer): Change link

--- client/src/library/components/Footer/index.jsx
+++ client/src/library/components/Footer/index.jsx
@@ -1,35 +1,54 @@
 import React from 'react';
-import './style.css'
+import './style.css';
 import '../../../resources/styles/global.css';
 
 function Footer(){
 	return (
-		<footer class="shadow dark:bg-gray-900">
-			<div class="w-full max-w-screen-xl mx-auto md:py-8">
-				<div class="sm:flex sm:items-center sm:justify-between">
-					<a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+		<footer class='shadow dark:bg-gray-900'>
+			<div class='w-full max-w-screen-xl mx-auto md:py-8'>
+				<div class='sm:flex sm:items-center sm:justify-between'>
+					<a
+						href='https://flowbite.com/'
+						class='flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse'
+					>
 						{/* <img src="" class="h-8" alt="Flowbite Logo" /> */}
-						<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Logo</span>
+						<span class='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
+							Logo
+						</span>
 					</a>
-					<ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+					<ul class='flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400'>
 						<li>
-							<a href="/" class="hover:underline me-4 md:me-6">About</a>
+							<a href='/' class='hover:underline me-4 md:me-6'>
+								About
+							</a>
 						</li>
 						<li>
-							<a href="/" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+							<a href='/' class='hover:underline me-4 md:me-6'>
+								Privacy Policy
+							</a>
 						</li>
 						<li>
-							<a href="/" class="hover:underline me-4 md:me-6">Licensing</a>
+							<a href='/' class='hover:underline me-4 md:me-6'>
+								Licensing
+							</a>
 						</li>
 						<li>
-							<a href="/" class="hover:underline">Contact</a>
+							<a href='/' class='hover:underline'>
+								Contact
+							</a>
 						</li>
 					</ul>
 				</div>
-				<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-				<span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+				<hr class='my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8' />
+				<span class='block text-sm text-gray-500 sm:text-center dark:text-gray-400'>
+					© 2023{' '}
+					<a href='https://andreimelo.netlify.app/' class='hover:underline'>
+						Melo™
+					</a>. All Rights Reserved.
+				</span>
 			</div>
-	</footer>);
+		</footer>
+	);
 }
 
 export default React.memo(Footer);
