6187a5a879979bb9ea06f515156f5b8ffc019c1b
andreimelo <andreiagno.melo@gmail.com>
2024-03-05 22:26:25 -0600

    chore: Code split new arrivals in home

---
+++ client/src/pages/default/Home/NewArrivals/NewArrivals.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { getProductsBySortAndOrder } from '../../../../library/services/product';
+import Card from '../../../../library/components/Card';
+
+const NewArrivals = () => {
+	const [
+		products,
+		setProducts,
+	] = useState();
+	const [
+		loading,
+		setLoading,
+	] = useState(false);
+
+	async function fetchProductsByCount(){
+		try {
+			setLoading(true);
+			const result = await getProductsBySortAndOrder('createdAt', 'desc', 3);
+			setProducts(result);
+			setLoading(false);
+		} catch (error) {
+			alert(error);
+			setLoading(false);
+		}
+	}
+
+	useEffect(() => {
+		fetchProductsByCount();
+	}, []);
+	return (
+		<div>
+			<label className='text-2xl font-semibold'>New Arrivals</label>
+
+			{
+				loading ? <h2>🌀 Loading....</h2> :
+				<div className='grid grid-cols-3 gap-4'>
+					{products &&
+						products.map((item) => (
+							<Card
+								imgSrc={item.images}
+								title={item.title}
+								description={item.description}
+								slug={item.slug}
+								linkTo={'/product'}
+								isProductAndCart
+							/>
+						))}
+				</div>}
+		</div>
+	);
+};
+
+export default NewArrivals;
