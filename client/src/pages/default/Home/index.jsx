dfdfc67a327489c5312e45bffe1af1e7b4832147
andreimelo <andreiagno.melo@gmail.com>
2024-03-05 22:32:53 -0600

    feat(home): Added best sellers component in home

--- client/src/pages/default/Home/index.jsx
+++ client/src/pages/default/Home/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import NewArrivals from './NewArrivals';
+import BestSellers from './BestSellers';
 
 const Home = () => {
 	return (
 		<div className='w-full max-w-screen-xl mx-auto whitespace-pre-wrap break-words'>
 			<div className='flex my-5'>
 				<NewArrivals />
+			</div>
+			<div className='flex my-5'>
+				<BestSellers />
 			</div>
 		</div>
 	);
