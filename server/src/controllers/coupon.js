adf62f7716b306335796197f64d97e612c548323
andreimelo <andreiagno.melo@gmail.com>
2024-04-22 15:00:48 -0500

    feat: Add create, remove, list of coupon in admin in services/client and server

---
+++ server/src/controllers/coupon.js
@@ -0,0 +1,38 @@
+const Coupon = require('../models/coupon');
+
+exports.create = async (req, res) => {
+	try {
+		const { name, expiry, discount } = req.body;
+		const createCoupon = await Coupon({ name, expiry, discount }).save();
+		res.json({
+			data : createCoupon,
+			ok   : true,
+		});
+	} catch (error) {
+		res.status(400).json(err);
+	}
+};
+
+exports.remove = async (req, res) => {
+	try {
+		const removeCoupon = await Coupon.findByIdAndDelete(req.params.couponId).exec();
+		res.json({
+			data : removeCoupon,
+			ok   : true,
+		});
+	} catch (error) {
+		res.status(400).json(error);
+	}
+};
+
+exports.list = async (req, res) => {
+	try {
+		const listCoupon = await Coupon.find({}).sort({ createdAt: -1 }).exec();
+		res.json({
+			data : listCoupon,
+			ok   : true,
+		});
+	} catch (error) {
+		res.status(400).json(error);
+	}
+};
