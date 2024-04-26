b7a4bef6a5fe12192b953b63fcb874de9b94f0f1
andreimelo <andreiagno.melo@gmail.com>
2024-04-25 23:45:33 -0500

    refactor: coupon layout design

--- client/src/pages/admin/Coupon/Coupon.jsx
+++ client/src/pages/admin/Coupon/Coupon.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Sidebar from '../../../library/components/SideBar';
+import Input from '../../../library/components/Input';
 
 const Coupon = ({ role }) => {
 	return (
@@ -10,6 +11,31 @@ const Coupon = ({ role }) => {
 				</div>
 				<div className='flex-auto w-64 mx-10'>
 					<label className='text-2xl font-semibold'>Create Coupon</label>
+					<form className='grid my-5'>
+						<Input
+							placeHolder='Enter a name'
+							variant={
+								'mb-5 bg-white-200 appearance-none border border-gray-300 rounded w-2/4 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+							}
+						/>
+						<Input
+							placeHolder='Enter a discount'
+							variant={
+								'mb-5 bg-white-200 appearance-none border border-gray-300 rounded w-2/4 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+							}
+						/>
+
+						<Input
+							placeHolder='Enter a date'
+							type={'date'}
+							variant={
+								'mb-5 bg-white-200 appearance-none border border-gray-300 rounded w-2/4 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+							}
+						/>
+						<button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 border border-blue-700 rounded text-sm w-1/4'>
+							Submit
+						</button>
+					</form>
 				</div>
 			</div>
 		</div>
