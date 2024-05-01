195b31457d47f78518470b7d2cb7afce3058b9c1
andreimelo <andreiagno.melo@gmail.com>
2024-05-01 14:58:07 -0500

    feat(checkout&payment): Add coupon reducer and scenarios if cart is empty and greater than 0 checkout page should not be access, redirect to payment in place order button and Create route to payment

---
+++ client/src/pages/default/Payment/Payment.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Payment = () => {
+    return (
+        <div className='w-full max-w-screen-xl mx-auto'>
+            Welcome to payment
+        </div>
+    )
+}
+
+export default Payment;
