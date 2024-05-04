e90915f43cbb7ea245053da40c8712de8e7e2d18
andreimelo <andreiagno.melo@gmail.com>
2024-05-03 19:41:37 -0500

    feat(order): Create order schema in model /server

---
+++ server/src/models/order.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+const orderSchema = new mongoose.Schema(
+	{
+		products      : [
+			{
+				product : {
+					type : ObjectId,
+					ref  : 'Product',
+				},
+				count   : Number,
+				color   : String,
+			},
+		],
+		paymentIntent : {},
+		orderStatus   : {
+			type    : String,
+			default : 'Not Processed',
+			enum    : [
+				'Not yet processed',
+				'Ongoing process',
+				'Order has been dispatched',
+				'Out for delivery',
+				'Order has been cancelled',
+				'Order is completed',
+			],
+		},
+		orderedBy     : { type: ObjectId, ref: 'User' },
+	},
+	{ timestamps: true },
+);
+
+module.exports = mongoose.model('Order', orderSchema);
