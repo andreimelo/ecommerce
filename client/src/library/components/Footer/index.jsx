e4328621cd783d4156f03b786ef65eb560cc9bb1
andreimelo <andreiagno.melo@gmail.com>
2024-06-07 23:18:42 -0500

    refactor: Remove unnecessary comments

--- client/src/library/components/Footer/index.jsx
+++ client/src/library/components/Footer/index.jsx
@@ -11,7 +11,6 @@ function Footer(){
 						href='https://flowbite.com/'
 						className='flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse'
 					>
-						{/* <img src="" class="h-8" alt="Flowbite Logo" /> */}
 						<span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
 							Logo
 						</span>
