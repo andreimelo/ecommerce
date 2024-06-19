1c2d5f410a24b7d115c2bdf1eb22eb578212b816
andreimelo <andreiagno.melo@gmail.com>
2024-06-18 23:37:05 -0500

    style: Add mobile view to profile view and renaming files

--- client/src/pages/user/ChangePassword/ChangePassword.jsx
+++ client/src/pages/user/ChangePassword/ChangePassword.jsx
@@ -5,8 +5,8 @@ import PropTypes from 'prop-types';
 function ChangePassword({ role }){
 	return (
 		<div className='w-full max-w-screen-xl mx-auto'>
-			<div className='flex my-10'>
-				<div className='flex-none w-40 border-r border-gray-200'>
+			<div className='flex my-10 max-[600px]:items-baseline'>
+				<div className='flex-none w-40 border-r border-gray-200 max-[600px]:border-none'>
 					<SideBar role={role} />
 				</div>
 				<div className='flex-auto w-64 mx-10'>
