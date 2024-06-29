6b9cb73abf06c27277b886be9dac80e004433883
andreimelo <andreiagno.melo@gmail.com>
2024-06-28 23:48:24 -0500

    refactor: Renaming alt props image in header

--- client/src/resources/images/index.js
+++ client/src/resources/images/index.js
@@ -2,6 +2,6 @@ import defaultImage from '../images/default.webp';
 import logo from '../images/melo-logo.svg';
 
 export const images = {
-	default     : defaultImage,
-	productLogo : logo,
+	default   : defaultImage,
+	brandLogo : logo,
 };
